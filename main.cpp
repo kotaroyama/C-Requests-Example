817e8689421a063ebb3cf2c243fc54bc7f478dee
Kotaro Yama <kotaro.h.yama@gmail.com>
2020-07-10 04:07:52 -0700

    Third commit

--- main.cpp
+++ main.cpp
@@ -5,11 +5,7 @@
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 
-<<<<<<< HEAD
-using json = nlohmann::json
-=======
 using json = nlohmann::json;
->>>>>>> 1967f3d... Second commit
 
 int main(int argc, char** argv) {
   std::string city = "Davis";
